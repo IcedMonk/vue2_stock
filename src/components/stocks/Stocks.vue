a0b125a1fbd24a0b3774231959dacea53476e7bc
Samyek Sonawane <samyek@Samyeks-MacBook-Air.local>
2019-03-24 03:24:48 +0530

    Added the pratice data stocks array and loaded the Stock.vue component here to populate it with the data & added for loop to iterate through the data array

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,5 +1,24 @@
 <template>
   <div>
-    <h1>stock - stocks route</h1>
+    <app-stock v-for="stock in stocks"></app-stock>
   </div>
 </template>
+
+
+<script>
+import Stock from './Stock.vue';
+export default {
+  data() {
+    return {
+      stocks: [
+        {id: 1, name: 'BMW', price: 5},
+        {id: 2, name: 'Porche', price: 2},
+        {id: 3, name: 'Lamborghini', price: 9}
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
