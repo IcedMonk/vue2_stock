bdabc30b0ba6609266272645e98bdb215e468c9e
Samyek Sonawane <samyek@Samyeks-MacBook-Air.local>
2019-03-24 15:42:43 +0530

    added the disable button on ln:18

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,14 +2,21 @@
   <div class="col-sm-6 col-md-4">
     <div class="panel panel-success">
       <div class="panel-heading">
-        <h3 class="panel-title">{{stock.name}} <small>Price : {{stock.price}}</small></h3>
+        <h3 class="panel-title">
+          {{stock.name}}
+          <small>Price : {{stock.price}}</small>
+        </h3>
       </div>
       <div class="panel-body">
         <div class="pull-left">
           <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
         </div>
         <div class="pull-right">
-          <button class="btn btn-success" @click="buyStock">Buy</button>
+          <button
+            class="btn btn-success"
+            @click="buyStock"
+            :disabled="quantity <= 0 || Number.isInteger(quantity)"
+          >Buy</button>
         </div>
       </div>
     </div>
@@ -18,11 +25,11 @@
 
 <script>
 export default {
-  props: ['stock'],
+  props: ["stock"],
   data() {
     return {
       quantity: 0
-    }
+    };
   },
   methods: {
     buyStock() {
@@ -35,6 +42,6 @@ export default {
       this.quantity = 0;
     }
   }
-}
+};
 </script>
 
